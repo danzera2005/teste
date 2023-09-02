bb0ac93c1a1f8dacb63064a293995c4771f9009c
danilo silva <ds5695749@gmail.com>
2023-09-02 11:34:52 -0300

    troca de porta

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require("express")
 const app = express()
-const port = 3000
 
 
-app.listen(port,(err)=>{
-    if(err) throw err;
-    console.log(`Servidor rodando na porta ${port}`)
-})
+app.listen(process.env.PORT || 3000)
 
 app.get('/',(req,res)=>{
     res.send("ola")
