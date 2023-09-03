2a50fd0e7e039f0aad88784086ecee4209fb9021
danilo silva <ds5695749@gmail.com>
2023-09-03 15:12:39 -0300

    rebuild

--- index.js
+++ index.js
@@ -2,26 +2,12 @@ const express = require("express")
 const app = express()
 const Route = require("./src/Router")
 const conn = require("./config/conexao")
-const cors = require("cors")
-const session = require("express-session")
-const fileupload = require("express-fileupload")
-const fs = require("fs")
-const path = require("path")
+
 
 
 app.use(Route)
 app.listen(process.env.PORT || 5000)
 
-app.use(session({secret: 'jjokockokxzokxkpczxpkpkpdadsdafsadsdsdafasfsffadvadd'}))
-app.use(express.json())
-app.use(fileupload({
-    useTempFiles:true,
-    tempFileDir: path.join(__dirname, 'temp')
-}))
-app.use(cors({
-    origin: "http://localhost:3000",
-}))
-express.urlencoded()
 
 
 conn.connect((err) =>{
